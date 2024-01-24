708629cb950dabc020e3136a8a61d5fd9a90b065
Christian Samson <christiansamson@Christians-MBP.ptd.net>
2024-01-24 13:11:22 -0500

    Added proxy and concurrent codes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import cookieParser from "cookie-parser";
 // routers
 import jobRouter from "./routers/jobRouter.js";
 import authRouter from "./routers/authRouter.js";
+import userRouter from "./routers/userRouter.js";
 
 // middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -26,8 +27,14 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//temp
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
+});
+
 // API routes setup
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 // Not found error
