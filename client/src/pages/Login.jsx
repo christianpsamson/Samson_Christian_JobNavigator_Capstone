56ce277673250518ff9f06b1cc773c0230578b4f
Christian Samson <christiansamson@Christians-MBP.ptd.net>
2024-01-24 17:52:08 -0500

    Added react-toastify, updated Login and Register pages for POST request

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,25 +1,46 @@
 import React from "react";
-import { Logo, FormRow } from "../components";
+import { Link, Form, redirect, useNavigation } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
-import { Link, NavLink } from "react-router-dom";
+import { FormRow, Logo } from "../components";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post("/auth/login", data);
+    toast.success("Login successful");
+    return redirect("/dashboard");
+  } catch {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const Login = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Login</h4>
         <FormRow type="email" name="email" defaultValue="" />
         <FormRow type="password" name="password" defaultValue="" />
         <button className="btn btn-block">Login</button>
-        <button className="btn btn-block">Take a Tour</button>
+        <button className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "Take A Tour"}
+        </button>
         <p>
           New to Carrera?{" "}
           <Link to="/register" className="login-signup">
             Sign up
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,27 +1,53 @@
+import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import React from "react";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
-import { Link } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+// Extracting data from form & making POST request to "api/v1/auth/register"
+// then redirect to /login after registration
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post("/auth/register", data);
+    toast.success("Registration successful");
+    return redirect("/login");
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const Register = () => {
+  // useNavigation for busy indicator, used in submit button
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
-        <FormRow type="text" name="name" defaultValue="" />
+        <FormRow type="text" name="name" defaultValue="John" />
         {/* prettier-ignore */}
-        <FormRow type="text" name="lastname" labelText="Last Name" defaultValue=""/>
-        <FormRow type="email" name="email" defaultValue="" />
-        <FormRow type="password" name="password" defaultValue="" />
-        <button className="btn btn-block">submit</button>
+        <FormRow type="text" name="lastName" labelText="Last Name" defaultValue="Smith"/>
+        <FormRow type="text" name="location" defaultValue="New York" />
+        <FormRow type="email" name="email" defaultValue="jsmith@yahoo.com" />
+        <FormRow type="password" name="password" defaultValue="password123" />
+        <button className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "submit"}
+        </button>
         <p>
           Already have an account?{" "}
           <Link to="/login" className="login-signup">
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
